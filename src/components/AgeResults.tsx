7509d4f63aec3f10d4436542f84b10b750473d3e
Jonathan <jonathanpetersen2016@gmail.com>
2023-04-07 19:53:00 -0300

    Time traveler error

--- src/components/AgeResults.tsx
+++ src/components/AgeResults.tsx
@@ -8,22 +8,42 @@ type ageObj = {
 
 const AgeResults: React.FC<{ data: ageObj }> = (props) => {
   const data = props.data;
+  let condition = true;
+  if (data.years !== "--" && +data.years > 0) {
+    condition = true;
+  } else if (+data.years < 0) {
+    condition = false;
+  }
+
   return (
-    <div className="results-container">
-      <div>
-        <span className="age-text text__highlight">{data.years}</span>
-        <span className="age-text">{+data.years !== 1 ? "years" : "year"}</span>
-      </div>
-      <div>
-        <span className="age-text text__highlight">{data.months}</span>
-        <span className="age-text">
-          {+data.months !== 1 ? "months" : "month"}
-        </span>
-      </div>
-      <div>
-        <span className="age-text text__highlight">{data.days}</span>
-        <span className="age-text">{+data.months !== 1 ? "days" : "day"}</span>
-      </div>
+    <div>
+      {+data.years < 0 && (
+        <div>
+          <h2>Are you perhaps a time traveler?!</h2>
+        </div>
+      )}
+      {condition && (
+        <div className="results-container">
+          <div>
+            <span className="age-text text__highlight">{data.years}</span>
+            <span className="age-text">
+              {+data.years !== 1 ? "years" : "year"}
+            </span>
+          </div>
+          <div>
+            <span className="age-text text__highlight">{data.months}</span>
+            <span className="age-text">
+              {+data.months !== 1 ? "months" : "month"}
+            </span>
+          </div>
+          <div>
+            <span className="age-text text__highlight">{data.days}</span>
+            <span className="age-text">
+              {+data.months !== 1 ? "days" : "day"}
+            </span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
